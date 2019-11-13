2bdf00e483f3711ca23a84045c61eda57d0e065e
Ignacet <igna_cet@yahoo.com.ar>
2019-11-13 14:25:43 -0300

    Importacion de Funciones

---
+++ funcmat.py
@@ -0,0 +1,21 @@
+""" Funcion suma: (int,int) -> int
+    uso: suma(2,3)
+
+    Funcion resta: (int,int) -> int
+    uso: resta(3,2)
+"""
+
+def suma (_val1,_val2):
+    return _val1+_val2
+
+def resta(_val1,_val2):
+    return _val1-_val2
+
+
+def main():
+    print ("suma: ",suma(3,3))
+    print ("resta: ",resta(3,2))
+
+if __name__ == "__main__":
+    main()
+
---
+++ import_funcmat.py
@@ -0,0 +1,8 @@
+import funcmat
+
+suma = funcmat.suma(4,5)
+resta = funcmat.resta(5,7)
+
+print("suma ", suma)
+print("resta ", resta)
+
