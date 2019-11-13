606039e04e6cec93cc07a30683559aa200d7b344
Ignacet <igna_cet@yahoo.com.ar>
2019-11-13 10:35:12 -0300

    ejercicio 2b.5

--- Ej_2b.py
+++ Ej_2b.py
@@ -1,18 +1,31 @@
 
-def check_Number(x):
-    if (0 <= x <= 100):
-        return x 
+def check_Number(_x):
+    if (0 <= _x <= 100):
+        return _x 
     else:
         return -1 
 
-def avgNumber (a1,a2,a3):
-    return (a1+a2+a3)/3
+def avgNumber (_a1,_a2,_a3):
+    return (_a1+_a2+_a3)/3
+
+valores = []
 
 a = input("Ingrese un numero:")
 b = input("Ingrese un numero:")
 c = input("Ingrese un numero:")
+d = input("Ingrese un numero:")
 
-if (check_Number(a) * check_Number(b) * check_Number(c) < 0):
+valores.insert(0,check_Number(a))
+valores.insert(0,check_Number(b))
+valores.insert(0,check_Number(c))
+valores.insert(0,check_Number(d))
+
+print (valores)
+valores.sort()
+
+result = check_Number(valores[1]) * check_Number(valores[2]) * check_Number(valores[3])
+
+if ( result < 0):
     print "ERROR"
 else:
-    print avgNumber(check_Number(a),check_Number(b),check_Number(c))
+    print avgNumber(check_Number(valores[1]),check_Number(valores[2]),check_Number(valores[3]))
