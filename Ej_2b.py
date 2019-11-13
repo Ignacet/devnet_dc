5b4be7d1a3f5c19d0af40aa00c36d7c17fbec8ae
Ignacet <igna_cet@yahoo.com.ar>
2019-11-13 11:16:40 -0300

    ejercicio 2b.6

--- Ej_2b.py
+++ Ej_2b.py
@@ -1,4 +1,9 @@
 
+def week_elapsed (_dayini, _dayend):
+    days = abs(_dayini-_dayend)
+    return days/7
+
+
 def check_Number(_x):
     if (0 <= _x <= 100):
         return _x 
@@ -9,6 +14,10 @@ def avgNumber (_a1,_a2,_a3):
     return (_a1+_a2+_a3)/3
 
 valores = []
+print week_elapsed (8,5)
+print week_elapsed (3,20)
+print week_elapsed (20,3)
+
 
 a = input("Ingrese un numero:")
 b = input("Ingrese un numero:")
@@ -29,3 +38,4 @@ if ( result < 0):
     print "ERROR"
 else:
     print avgNumber(check_Number(valores[1]),check_Number(valores[2]),check_Number(valores[3]))
+
